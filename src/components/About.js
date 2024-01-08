d8bab4ea27a0b7f5124afb25b0e291db119a8485
black_pomeranian <ryo.roma.16@gmail.com>
2024-01-08 14:40:59 +0900

    自己紹介テキスト追加

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <h1>Black Pomeranian</h1>
+    <div>
+        <h1>Black Pomeranian</h1>
+        <p>プログラマ、VJ。オーディオビジュアル、XR、インタラクティブ作品等の制作を行う。
+            認知を変容させる表現をテーマに、表現や技術について実験的なアプローチを実施。
+            このアプローチを通じて、文化への貢献を志向している。</p>
+    </div>
   )
 }
 
