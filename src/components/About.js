b8599a8fc55abdbdef62213a8982f12b17bea2ed
black_pomeranian <ryo.roma.16@gmail.com>
2024-01-08 17:25:34 +0900

    画像のパスを変更

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import profileImage from "../images/profile.png"
 
 const About = () => {
   return (
     <div>
         <h1>Black Pomeranian</h1>
-        <img src={profileImage} alt="Profile-Image" />
+        <img src={`${process.env.PUBLIC_URL}/images/profile.png`} />
         <p>プログラマ、VJ。オーディオビジュアル、XR、インタラクティブ作品等の制作を行う。
             認知を変容させる表現をテーマに、表現や技術について実験的なアプローチを実施。
             このアプローチを通じて、文化への貢献を志向している。
