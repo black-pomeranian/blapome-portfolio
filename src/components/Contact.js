bf57cdc0632a3933043e1b3b7680e1a811f882ea
black_pomeranian <ryo.roma.16@gmail.com>
2024-01-12 23:42:30 +0900

    行の間隔

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,9 @@ const Contact = () => {
     <div className="container">
       <div className="row justify-content-center">
         <div className="col-auto text-left">
-          <p>black.pomera@gmail.com</p>
-          <a href="https://twitter.com/black_pomerania">X (Twitter)</a><br />
-          <a href="https://www.instagram.com/_black_pomeranian/">Instagram</a>
+          <div className="mb-2">black.pomera@gmail.com</div> {/* mb-2 は下マージンを設定 */}
+          <div className="mb-2"><a href="https://twitter.com/black_pomerania">X (Twitter)</a></div>
+          <div><a href="https://www.instagram.com/_black_pomeranian/">Instagram</a></div>
         </div>
       </div>
     </div>
