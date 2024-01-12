42a775aef1d0626aad71b8791274232c22f9bc8b
black_pomeranian <ryo.roma.16@gmail.com>
2024-01-12 23:39:44 +0900

    文字の配置修正

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const Contact = () => {
   return (
-    <div>
-      <div class="container">
-        <div class="row">
-          <div class="col-12"><p>black.pomera@gmail.com</p></div>
-        </div>
-        <div class="row">
-          <div class="col-12"><a href="https://twitter.com/black_pomerania">X (Twitter)</a></div>
-        </div>
-        <div class="row">
-          <div class="col-12"><a href="https://www.instagram.com/_black_pomeranian/">Instagram</a></div>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-auto text-left">
+          <p>black.pomera@gmail.com</p>
+          <a href="https://twitter.com/black_pomerania">X (Twitter)</a><br />
+          <a href="https://www.instagram.com/_black_pomeranian/">Instagram</a>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
