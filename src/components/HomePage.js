7b194477c67dc46212e57bd76392b5d68b959d01
black_pomeranian <ryo.roma.16@gmail.com>
2024-02-18 20:34:36 +0900

    3Dモデル入れ替え、配置

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -10,67 +10,76 @@ const HomePage = () => {
   useEffect(() => {
     // シーン、カメラ、レンダラーの初期化
     const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x404040);
+    // 3. カメラを作成する
+    const camera = new THREE.PerspectiveCamera(
+        75, // 視野角
+        window.innerWidth / window.innerHeight, // アスペクト比
+        0.1, // ニアクリップ面
+        1000 // ファークリップ面
+    );
+    camera.position.z = 0.5; // カメラの位置を設定する
 
-        // 3. カメラを作成する
-        const camera = new THREE.PerspectiveCamera(
-            75, // 視野角
-            window.innerWidth / window.innerHeight, // アスペクト比
-            0.1, // ニアクリップ面
-            1000 // ファークリップ面
-        );
-        camera.position.z = 15; // カメラの位置を設定する
+    // 4. レンダラーを作成する
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-        // 4. レンダラーを作成する
-        const renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(renderer.domElement);
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.zoomSpeed = 0.5;
+    controls.update(); // 初期化時に一度だけ呼び出す
 
-        const controls = new OrbitControls(camera, renderer.domElement);
-        controls.zoomSpeed = 0.5;
-        controls.update(); // 初期化時に一度だけ呼び出す
+    // AmbientLightを追加する
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
 
-        // AmbientLightを追加する
-        const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-        scene.add(ambientLight);
+    // スポットライトを追加
+    const spotLight = new THREE.SpotLight(0xffffff, 1);
+    spotLight.position.set(-1, 2, 1); // 斜め左上からの位置
+    scene.add(spotLight);
 
-        // 5. GLTFLoaderを作成する
-        const loader = new GLTFLoader();
-        
-        const dracoLoader = new DRACOLoader(); // DRACOLoaderのインスタンスを作成
-        dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.4.1/');
-        loader.setDRACOLoader(dracoLoader); // GLTFLoaderにDRACOLoaderのインスタンスを提供
+    // 5. GLTFLoaderを作成する
+    const loader = new GLTFLoader();
 
-        // 6. 3Dモデルのロード＆描画
-        loader.load(
-          `${process.env.PUBLIC_URL}/model/blapome_far.glb`,
-            function (gltf) {
-                scene.add(gltf.scene);
-            },
-            undefined,
-            function (error) {
-                console.error(error);
-            }
-        );
+    const dracoLoader = new DRACOLoader(); // DRACOLoaderのインスタンスを作成
+    dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.4.1/');
+    loader.setDRACOLoader(dracoLoader); // GLTFLoaderにDRACOLoaderのインスタンスを提供
 
-        function animate() {
-            requestAnimationFrame(animate);
-            renderer.render(scene, camera);
+    // 6. 3Dモデルのロード＆描画
+    let model = null;
+    loader.load(
+      `${process.env.PUBLIC_URL}/model/blapome_far_mesh.glb`,
+        function (gltf) {
+            model = gltf.scene;
+            model.position.set(0,0,0)
+            model.rotation.set(Math.PI/2,Math.PI/2,0); 
+            scene.add(model);
+        },
+        undefined,
+        function (error) {
+            console.error(error);
         }
-        animate();
+    );
 
-    // リサイズイベントのハンドリング
-    window.addEventListener('resize', () => {
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-    });
+    function animate() {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+    }
+    animate();
 
-    // クリーンアップ関数
-    return () => {
-    };
-  }, []);
+  // リサイズイベントのハンドリング
+  window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+  });
 
-  return <div ref={ref}></div>;
+  // クリーンアップ関数
+  return () => {
+  };
+}, []);
+
+return <div ref={ref}></div>;
 }
 
 export default HomePage;
