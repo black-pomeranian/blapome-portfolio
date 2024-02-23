cf7363b78fa56314b902f3451a56b882f4fbe9b8
black_pomeranian <ryo.roma.16@gmail.com>
2024-02-23 15:12:23 +0900

    リンクを別タブで開く

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,8 +6,8 @@ const Contact = () => {
       <div className="row h-75 justify-content-center align-items-center">
         <div className="col-auto text-left ">
           <p className="mail mb-3"><a href="mailto:black.pomera@gmail.com" >black.pomera@gmail.com</a></p> 
-          <p className="sns mb-3 "><a href="https://twitter.com/black_pomerania">X (Twitter)</a></p>
-          <p className="sns"><a href="https://www.instagram.com/_black_pomeranian/">Instagram</a></p>
+          <p className="sns mb-3 "><a href="https://twitter.com/black_pomerania" target="_blank">X (Twitter)</a></p>
+          <p className="sns"><a href="https://www.instagram.com/_black_pomeranian/" target="_blank">Instagram</a></p>
         </div>
       </div>
     </div>
