be5756f4ec02c071d4965710e9be468dc93bdeb8
black_pomeranian <ryo.roma.16@gmail.com>
2024-02-24 01:58:14 +0900

    背景色変更

--- src/components/Project.js
+++ src/components/Project.js
@@ -20,8 +20,7 @@ function Projects() {
             <Link to={image.url}>
               <div className="image-overlay-container card-aspect-ratio">
                 <img src={image.src} alt={image.alt} className="img-fluid img-rounded" />
-
-                <div className="image-overlay project-img-title"></div>
+                <div className="image-overlay"></div>
               </div>
             </Link>
             <p className="project-title">{image.title}</p>
