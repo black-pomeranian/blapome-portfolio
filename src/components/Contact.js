cd54e023cf733f1af7459f08d3ef3d70ad41f8ee
black_pomeranian <ryo.roma.16@gmail.com>
2024-02-23 15:09:12 +0900

    メールリンクを作成

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
     <div className="container h-100 text-light">
       <div className="row h-75 justify-content-center align-items-center">
         <div className="col-auto text-left ">
-          <p className="mail mb-3">black.pomera@gmail.com</p> 
+          <p className="mail mb-3"><a href="mailto:black.pomera@gmail.com" >black.pomera@gmail.com</a></p> 
           <p className="sns mb-3 "><a href="https://twitter.com/black_pomerania">X (Twitter)</a></p>
           <p className="sns"><a href="https://www.instagram.com/_black_pomeranian/">Instagram</a></p>
         </div>
