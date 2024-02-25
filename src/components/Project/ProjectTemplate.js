e95c6f6473f4ac70e79c11b4b10b202cde5948bc
black_pomeranian <ryo.roma.16@gmail.com>
2024-02-25 22:39:36 +0900

    LoM追加

--- src/components/Project/ProjectTemplate.js
+++ src/components/Project/ProjectTemplate.js
@@ -24,18 +24,18 @@ const ProjectTemplate = () => {
 
         {/* 説明 */}
         <div className="row justify-content-center">
-          <div className="descliption-jp col-12 col-md-8">
+          <div className="descliption-jp col-12 col-md-8 py-2">
             <p>
               説明文がここに入ります。
             </p>
           </div>
-          <div className="descliption-en col-12 col-md-8">
+          <div className="descliption-en col-12 col-md-8 py-2">
             <p>
               Here is Explanation
             </p>
           </div>
 
-          <div className="credit col-12 col-md-8">
+          <div className="credit col-12 col-md-8 py-2">
             <p>
               Credit<br></br>
               aaa：Black Pomeranian<br></br>
