6fa79f52bf65e2cac67164a2f0591aa260cf1b68
black_pomeranian <ryo.roma.16@gmail.com>
2024-02-26 17:26:17 +0900

    プロフィール変更

--- src/components/About.js
+++ src/components/About.js
@@ -18,15 +18,15 @@ const About = () => {
 
           <p className="profile-jp text-start mb-1 py-2">
             プログラマ、VJ。オーディオビジュアル、XR、インタラクティブ作品等の制作を行う。
-            構造の探求と、それに随伴する認知の変容をテーマに、表現や技術について実験的なアプローチを実施。
+            構造の探求と記号の交換による認知の変容をテーマに、表現や技術について実験的なアプローチを実施。
             このアプローチを通じて、文化への貢献を志向している。
           </p>
           <p className="profile-en text-start py-2">
-            Programmer and VJ, creating audiovisual, XR, and interactive works.
-            He conducts experimental approaches to expression and technology with the theme of expression that transforms perception.
+            I am a programmer and VJ. We create audiovisual, XR and interactive works.
+            I implement an experimental approach to expression and technology, based on the theme of cognitive transformation through the exploration of structure and the exchange of symbols.
             Through this approach, he aims to contribute to culture.
           </p>
-          <h3 className="about-performance text-start">出演歴</h3> 
+          <h3 className="about-performance text-start m-0">出演歴</h3> 
           <div className="performance-jp text-start py-2">
             <p className="performance">
               2024.02.16 さざれ場#5 <br></br>
--- src/components/Project/Portfolio.js
+++ src/components/Project/Portfolio.js
@@ -9,14 +9,14 @@ const Portfolio = () => {
         {/* タイトル */}
         <div className="row mb-4 justify-content-center">
           <div className="col-12 col-md-8">
-            <h1 className="project-title text-left">ポートフォリオサイトの開設</h1>
+            <h1 className="project-title text-left">Webサイトの開設</h1>
           </div>
         </div>
 
         {/* 画像 */}
         <div className="row mb-4 justify-content-center">
           <div className="col-12 col-md-8">
-            <img src={`${process.env.PUBLIC_URL}/images/profile.png`} alt="sample" className="img-fluid" />
+            <img src={`${process.env.PUBLIC_URL}/images/Portfolio/site.png`} alt="sample" className="img-fluid" />
           </div>
         </div>
 
@@ -24,12 +24,20 @@ const Portfolio = () => {
         <div className="row justify-content-center">
           <div className="descliption-jp col-12 col-md-8">
             <p>
-              説明文がここに入ります。
+              Webサイトを開設しました。<br></br>
+              よろしくお願いいたします。<br></br>
             </p>
           </div>
           <div className="descliption-en col-12 col-md-8">
             <p>
-              Here is Explanation
+              Web site is now open.<br></br>
+              Please take a look at it.<br></br>
+            </p>
+          </div>
+          <div className="credit col-12 col-md-8 py-2">
+            <p>
+              Credit<br></br>
+              All：Black Pomeranian<br></br>
             </p>
           </div>
         </div>
