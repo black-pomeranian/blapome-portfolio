90131e86e7e0dc7bcc1b81aabcab4cf1594185b0
Black Pomeranian <ryo.roma.16@gmail.com>
2024-12-29 18:16:56 +0900

    cssと文字列を修正

--- src/components/Performance.js
+++ src/components/Performance.js
@@ -5,18 +5,18 @@ const Performance = () => {
     <div className="container h-100 text-light ">
       <div className="flex justify-content-center align-items-center ">
           <div className="performance-jp text-start">
-            <ul className="performance list-unstyled space-y-8">
-              <li className="py-1">2024.11.16 upfn OLYMPICO Reception Party (DJ/VJ)</li>
-              <li className="py-1">2024.11.09 THEATER BOUNCE presented by イヨシコーラ(VJ)</li>
-              <li className="py-1">2024.10.07 PLAYGROUND vol.7 (VJ)</li>
-              <li className="py-1">2024.08.23 BENE×UMBRO POP UP STORE Mikawaya - Hamamatsu (VJ)</li>
-              <li className="py-1">2024.07.27 EN vol.1 "en"counter (DJ)</li>
-              <li className="py-1">2024.05.23 UMBRO 100th Anniversary BENE×UMBRO POPUP STORE RECEPTION PARTY (VJ)</li>
-              <li className="py-1">2024.03.30 PLAYGROUND vol.6 (VJ)</li>
-              <li className="py-1">2024.02.16 さざれ場#5 (VJ)</li>
-              <li className="py-1">2023.08.26 Jack in the box (DJ)</li>
-              <li className="py-1">2023.07.15 MID NIGHT ECHORS (VJ)</li>
-              <li className="py-1">2023.06.17 RIP CURRENT (DJ)</li>
+            <ul className="performance list-unstyled space-y-4">
+              <li>2024.11.16 [DJ/VJ] upfn OLYMPICO Reception Party</li>
+              <li>2024.11.09 [VJ] THEATER BOUNCE presented by イヨシコーラ</li>
+              <li>2024.10.07 [VJ] PLAYGROUND vol.7 </li>
+              <li>2024.08.23 [VJ] BENE×UMBRO POP UP STORE Mikawaya - Hamamatsu</li>
+              <li>2024.07.27 [DJ] EN vol.1 "en"counter</li>
+              <li>2024.05.23 [VJ] UMBRO 100th Anniversary BENE×UMBRO POPUP STORE RECEPTION PARTY</li>
+              <li>2024.03.30 [VJ] PLAYGROUND vol.6</li>
+              <li>2024.02.16 [VJ] さざれ場#5</li>
+              <li>2023.08.26 [DJ] Jack in the box</li>
+              <li>2023.07.15 [VJ] MID NIGHT ECHORS</li>
+              <li>2023.06.17 [DJ] RIP CURRENT</li>
             </ul>
           </div>
       </div>
