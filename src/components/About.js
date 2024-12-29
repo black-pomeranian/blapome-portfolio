053878261b94492a4e86784912dad9c797d1538e
Black Pomeranian <ryo.roma.16@gmail.com>
2024-12-29 18:04:34 +0900

    tailwind適用

--- src/components/About.js
+++ src/components/About.js
@@ -41,25 +41,6 @@ const About = () => {
               </ul>
             </div>
           </div>
-
-          <h3 className="about-performance text-start m-0 pt-2">Performance</h3> 
-          <div className="performance-jp text-start py-2">
-            <ul className="performance list-unstyled space-y-8">
-              <li className="py-1">2024.11.16 upfn OLYMPICO Reception Party (DJ/VJ)</li>
-              <li className="py-1">2024.11.09 THEATER BOUNCE presented by イヨシコーラ(VJ)</li>
-              <li className="py-1">2024.10.07 PLAYGROUND vol.7 (VJ)</li>
-              <li className="py-1">2024.08.23 BENE×UMBRO POP UP STORE Mikawaya - Hamamatsu (VJ)</li>
-              <li className="py-1">2024.07.27 EN vol.1 "en"counter (DJ)</li>
-              <li className="py-1">2024.05.23 UMBRO 100th Anniversary BENE×UMBRO POPUP STORE RECEPTION PARTY (VJ)</li>
-              <li className="py-1">2024.03.30 PLAYGROUND vol.6 (VJ)</li>
-              <li className="py-1">2024.02.16 さざれ場#5 (VJ)</li>
-              <li className="py-1">2023.08.26 Jack in the box (DJ)</li>
-              <li className="py-1">2023.07.15 MID NIGHT ECHORS (VJ)</li>
-              <li className="py-1">2023.06.17 RIP CURRENT (DJ)</li>
-            </ul>
-          </div>
-
-          
         </div>
       </div>
     </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,13 +2,23 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div className="container h-100 text-light">
-      <div className="row h-75 justify-content-center align-items-center">
-        <div className="col-auto text-left ">
-          <p className="mail mb-3"><a href="mailto:black.pomera@gmail.com" >black.pomera@gmail.com</a></p> 
-          <p className="sns mb-3 "><a href="https://twitter.com/black_pomerania" target="_blank" rel="noopener noreferrer">X (Twitter)</a></p>
-          <p className="sns"><a href="https://www.instagram.com/_black_pomeranian/" target="_blank" rel="noopener noreferrer">Instagram</a></p>
-        </div>
+    <div className="container h-100 text-light ">
+      <div className="flex justify-content-center align-items-center ">
+          <div className="performance-jp text-start">
+            <ul className="performance list-unstyled space-y-8">
+              <li className="py-1">2024.11.16 upfn OLYMPICO Reception Party (DJ/VJ)</li>
+              <li className="py-1">2024.11.09 THEATER BOUNCE presented by イヨシコーラ(VJ)</li>
+              <li className="py-1">2024.10.07 PLAYGROUND vol.7 (VJ)</li>
+              <li className="py-1">2024.08.23 BENE×UMBRO POP UP STORE Mikawaya - Hamamatsu (VJ)</li>
+              <li className="py-1">2024.07.27 EN vol.1 "en"counter (DJ)</li>
+              <li className="py-1">2024.05.23 UMBRO 100th Anniversary BENE×UMBRO POPUP STORE RECEPTION PARTY (VJ)</li>
+              <li className="py-1">2024.03.30 PLAYGROUND vol.6 (VJ)</li>
+              <li className="py-1">2024.02.16 さざれ場#5 (VJ)</li>
+              <li className="py-1">2023.08.26 Jack in the box (DJ)</li>
+              <li className="py-1">2023.07.15 MID NIGHT ECHORS (VJ)</li>
+              <li className="py-1">2023.06.17 RIP CURRENT (DJ)</li>
+            </ul>
+          </div>
       </div>
     </div>
   );
