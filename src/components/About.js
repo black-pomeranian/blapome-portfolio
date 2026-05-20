39bba2230fe2e86814d1e24c7848b3846b7678d8
Black Pomeranian <ryo.roma.16@gmail.com>
2026-05-21 00:59:53 +0900

    プロフィール更新

--- src/components/About.js
+++ src/components/About.js
@@ -20,24 +20,18 @@ const About = () => {
 
           <p className="profile-jp text-start mb-1 py-2">
             プログラマ / VJ / DJ。
-            クラブやライブの映像演出やデザインの制作、インタラクティブコンテンツの開発等を手掛ける。
-            プログラマとして、ゲーム、Web、AI、VRなど、視覚に関連するものを横断的に制作。
-            制作では、コードを中心としつつも、3DCG、実写、生成AIなどあらゆるツールを用いている。
-            VJとしては自作のVJソフトを用いてクラブやライブハウスでリアルタイムレンダリングの映像表現を行っている。
-            DJでは、テクノを軸としたレベルミュージックをプレイしている。
-            昼と夜、デジタルとアナログ、都市とローカル、人工物と自然などを越境する過程で残された記憶や感情をもとに活動している。
-            既存の仕組みの内部から立ち上がり、既存の仕組みに対するつまらなさを破壊したい。
+            インスタレーションを中心に、ゲーム、Web、AI、VR、映像など、視覚に関連するものを横断的に制作。
+            コードを中心としつつも、3DCG、実写、生成AIなどあらゆるツールを用いて制作。
+            VJとしては、自作のVJソフトを用いて都内のアンダーグラウンドなクラブやライブハウスで活動。
+            その際、音響解析の結果をその場で映像に反映するリアルタイムレンダリングの手法を主に用いている。
+            DJでは、暗く反復的なテクノの影響を強く受けつつも、ベースミュージックやレフトフィールド、ハウスなどジャンルレスにミックス。
 
           </p>
           <p className="profile-en text-start py-2">
             Programmer / VJ / DJ.
-            Engaged in video production and design production for clubs and live performances, as well as the development of interactive content.
-            As a programmer, I create games, web, AI, VR, and other visually related works in a cross-sectional manner.
-            In production, I mainly use code, but also utilize various tools such as 3DCG, live-action footage, and generative AI.
-            As a VJ, I use my own VJ software to create real-time rendered video expressions in clubs and live houses.
-            As a DJ, I play level music centered around techno.
-            My activities are based on memories and emotions left behind in the process of crossing over between day and night, digital and analog, urban and local, artificial and natural.
-            I want to rise from within existing systems and destroy the boredom towards them.
+            I create installations, games, web, AI, VR, videos, and more, using a wide range of tools including code, 3DCG, live-action footage, and generative AI.
+            As a VJ, I perform at underground clubs and live houses in Tokyo using my own VJ software, primarily utilizing real-time rendering techniques that reflect audio analysis results in the visuals on the spot.
+            As a DJ, I mix genre-less music influenced by dark and repetitive techno, bass music, leftfield, house, and more.
           </p>
 
           <div className="sns">
